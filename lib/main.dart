f174a86514e1e5f6a6be0dafe27e97c864d019b4
Vishal Chand <sam.chand96@gmail.com>
2025-11-30 19:50:03 +0530

    Created the list of transactions section, and some other functionality.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expense_tracker/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,6 +14,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State {
+  //temporary data of transactions
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "d1",
+      title: "New Laptop",
+      amount: 56897.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "d2",
+      title: "Daily needs",
+      amount: 5689,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "d3",
+      title: "Amazon order",
+      amount: 678,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,6 +48,84 @@ class _MyAppState extends State {
               color: Theme.of(context).colorScheme.inverseSurface,
             ),
           ),
+        ),
+        body: Column(
+          crossAxisAlignment: .stretch,
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Card(child: Text("Chart will come here")),
+            ),
+            Column(
+              children: [
+                ...transactions.map((trans) {
+                  return Card(
+                    child: Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 15,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.purple, width: 2),
+                          ),
+                          padding: EdgeInsets.all(10),
+                          child: Text(
+                            "₹${trans.amount}",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.purple,
+                            ),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: .start,
+                          children: [
+                            Text(
+                              trans.title,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              DateFormat.yMMMEd().format(trans.date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ],
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: .end,
+                  children: [
+                    TextField(
+                      decoration: InputDecoration(label: Text("Title")),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(label: Text("Amount")),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Add Transaction",
+                        style: TextStyle(color: Colors.deepPurple),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
